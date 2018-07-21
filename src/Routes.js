7b000bf724acbce8ed3a513f3fa1652100ae73c9
LigorioSalgado <salgadolg5@hotmail.com>
2018-07-20 21:50:08 -0500

    Agregando Single Movie

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import {
     BrowserRouter as Router,
-    Route
+    Route,Redirect
 } from 'react-router-dom';
 import React, { Component } from 'react';
 import Login from './components/Login/Login'
@@ -9,21 +9,33 @@ import Home from './components/Home/Home';
 import Nav from './components/Nav/Nav';
 import Logout from './components/Logout/Logout';
 import Movies from './components/Movies/Movies';
+import Movie from './components/Movie/Movie';
+import checkToken from './resolvers/checkToken';
 
 class Routes extends Component{
 
 
+    
     render(){
-        return(
 
+        const PrivateRoute = ({component:Component, ...rest}) => (
+            <Route {...rest} render = {(props) => (
+            checkToken() === true ? <Component {...props}/> : <Redirect to="/login"/>
+            )} />
+        )
+    
+        
+        return(
             <Router>
                 <main>
                     <Nav/>
                     <Route exact path='/' component={Home}/>
                     <Route exact path='/login' component={Login}/>
                     <Route exact path='/signup' component={Signup}/>
-                    <Route exact path='/logout' component={Logout}/>
-                    <Route exact path='/movies' component={Movies}/>
+                    <PrivateRoute exact path='/logout' component={Logout}/>
+                    <PrivateRoute path="/movies"  component={Movies}/>
+                    <PrivateRoute exact path='/movie/:id' component={Movie}/>
+
                 </main>
             </Router>
         )
